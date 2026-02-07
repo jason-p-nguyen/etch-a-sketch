4990feb6371735560efbc73c390b32fc0780ca20
Jason Nguyen <29442130+jason-p-nguyen@users.noreply.github.com>
2026-02-07 12:59:29 +0900

    Add container and div for grid
    Tested to make sure div worked as well

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
-// link css stylesheet
 // Create a 16x16 grid of square divs
-// Create divs using js.
-// Contain the grid squares in "container" div
-// Use flexbox to make divs appear as a grid
-// Beware of borders and margins, as they can adjust the size of the squares
+    // Create divs using js.
 
-// Set hover effect to change grid div colors when passed over
-// Add a new class for the hover
-// change the divs background using js
+    // Contain the grid squares in "container" div
+    const container = document.getElementById("container");
+    const div = document.createElement("div");
+    container.appendChild(div);
+    
+    // Use flexbox to make divs appear as a grid
+    // Beware of borders and margins, as they can adjust the size of the squares
+
+    // Set hover effect to change grid div colors when passed over
+    // Add a new class for the hover
+    // change the divs background using js
 
 // Add a button on the top of the screen
-// Create event listener for button 
-// prompt user for number of squares per side
-// existing grid should be removed
-// new grid should be created with requested number of squares
+    // Create event listener for button 
+    // prompt user for number of squares per side
+    // existing grid should be removed
+    // new grid should be created with requested number of squares
 // keep total space to 960px wide
 // limit user input to a max of 100
