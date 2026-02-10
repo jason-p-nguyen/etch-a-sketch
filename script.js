f7622359137b19ee4dfcc1ebeef2f5e05ba8a5d8
Jason Nguyen <29442130+jason-p-nguyen@users.noreply.github.com>
2026-02-10 13:35:14 +0900

    Create new pseudocode

---
+++ script.js
@@ -0,0 +1,31 @@
+// GET variable reference for container
+
+// Create a 16x16 grid of squares divs using a loop
+// This creates the row of the grid
+// BEGIN function grid(numOfSquares)
+// SET numOfSquares = 16
+// INITIALIZE i = 0
+// FOR (i <= numOfSquares) 
+// Increase i++
+    // This creates the column for each row square
+    // Initialize j = 0
+    // FOR (j <= i.length)
+    // Increase step by j++
+    // Create element ("div")
+    // Set classList square
+    // ADD div element to container
+    // OUTPUT grid to webpage
+
+// SET hover effect 
+// GET event listener to listen for mouseenter
+// BEGIN anon function
+    // ADD classList backGroundcolor
+    // Set backGroundColor in CSS
+    // OUTPUT new square with a background
+
+// GET variable reference for button
+// GET INPUT from user for numOfSquares
+// SET limit to 100
+// SET width limit to 960px in CSS
+// REMOVE current grid
+// Generate new grid based on the input 
